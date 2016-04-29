b415a0ee639066495a450a693441bb0437416884
unenglishable <unenglishable@gmail.com>
2016-04-28 16:12:16 -1000

    log socket options on connect fail

--- client/services/websocket.js
+++ client/services/websocket.js
@@ -5,15 +5,17 @@ module.exports = ['$window', 'Session',
   function($window, Session) {
     // Initiate the connection to the server
     var socketcluster = require('socketcluster-client');
-    var socket = socketcluster.connect({
+    var socketOptions = {
       rejectUnauthorized: false,
       secure: JSON.parse(forumData.websocket_secure),
       hostname: forumData.websocket_host,
       port: forumData.websocket_port,
       autoReconnect: true
-    })
+    };
+    var socket = socketcluster.connect(socketOptions)
     .on('error', function(err) {
       console.log('Websocket error:', err);
+      console.log(socketOptions);
     });
     return socket;
   }
